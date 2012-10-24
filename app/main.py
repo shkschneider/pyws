cbc6e8598cc7c6ffc2ff51d5265f15c5d334c3ae
shkschneider <shk.schneider@gmail.com>
2012-10-24 23:57:54 +0200

    Fixed the ERROR_TEMPLATE Script can be run using wsgiref server for debug

--- app/main.py
+++ app/main.py
@@ -5,15 +5,15 @@ import reply
 import sys
 
 bottle.BaseResponse.default_content_type = 'application/json; charset=UTF-8'
-bottle.ERROR_PAGE_TEMPLATE = reply.build(False, None)
+bottle.ERROR_PAGE_TEMPLATE = str(reply.build(False, None))
 
-URI_PARTS = {'name': '<name>'}
+URI_PARTS = {'name': '<name:re:[a-zA-Z]{6,24}>'}
 
 def route(uri, func, method='GET'):
     return [uri.format(**URI_PARTS), func, method]
 
-URI_MAP = [route('/', webservice.ws, 'GET'),
-           route('/hello/{name}', webservice.ws_hello, 'GET')]
+URI_MAP = [route('/', webservice.ws),
+           route('/hello/{name}', webservice.ws_hello)]
 
 APP = bottle.app()
 
@@ -21,6 +21,6 @@ for uri, func, method in URI_MAP:
     APP.route(uri, method=method, apply=[plugins.PreRequest])(func)
 
 if __name__ == '__main__':
-    sys.exit(1)
+    bottle.run(APP, server='wsgiref', port=9000, host='0.0.0.0')
 
 # EOF
